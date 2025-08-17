0ad6254ce934a70e8a1d3e508e316556b1e860c2
Siméon0477 <nouhoumimeon01@gmail.com>
2025-08-17 22:43:09 +0100

    menu

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@ using namespace std;
 
 Garage garage;
 int choix, ind;
+char immatriculation[10];
+Car *car;
+Motorcycle *motorcycle;
+Truck *truck;
 
 int main(){
     cout << "\033[35m******** Simulateur de Scores ******** \n\033[0m";
@@ -34,14 +38,62 @@ int main(){
                      << "\n 2 - Moto"
                      << "\n 3 - Camion"
                      << endl;
-                cin << vehicle_choice;
+                cin >> vehicle_choice;
 
                 //Enregistrement des informations
-                
+                char typeCarburant[20];
+                int annee, prix;
+                float poids;
+                cin.ignore();
+                cout << "Entrez le numéro d'imatriculation du véhicule : ";
+                cin.getline(immatriculation, 10);
+                cout << "Entrez le type de carburant du véhicule : ";
+                cin.getline(typeCarburant, 20);
+                cout << "Entrez l'année de production de votre véhicule' : ";
+                cin >> annee;
+                cout << "Entrez le prix d'achat de votre véhicule' : ";
+                cin >> prix;
+                cout << "Entrez le poids de votre véhicule' : ";
+                cin >> poids;
+
+                switch(vehicle_choice){
+                    case 1:
+                        char segment[20];
+                        int nombrePortes;
+                        cin.ignore();
+                        cout << "Entrez le segment de la voiture : ";
+                        cin.getline(segment, 20);
+                        cout << "Entrez le nombre de porte de la voiture' : ";
+                        cin >> nombrePortes;
+                        car = new Car(immatriculation, typeCarburant, annee, poids, prix, segment, nombrePortes); 
+                        garage.AddVehicle(car);
+                        break;
+                    case 2:
+                        char typeMoto[20];
+                        int nombreRoues;
+                        cin.ignore();
+                        cout << "Entrez le type de moto : ";
+                        cin.getline(typeMoto, 20);
+                        cout << "Entrez le nombre de roues de la moto' : ";
+                        cin >> nombreRoues;
+                        motorcycle = new Motorcycle(immatriculation, typeCarburant, annee, poids, prix, typeMoto, nombreRoues); 
+                        garage.AddVehicle(motorcycle);
+                        break;
+                    case 3:
+                        int chargeUtile;
+                        int nombreEssieux;
+                        cout << "Entrez la charge utile du camion' : ";
+                        cin >> chargeUtile;
+                        cout << "Entrez le nombre d'essieux du camion' : ";
+                        cin >> nombreEssieux;
+                        truck = new Truck(immatriculation, typeCarburant, annee, poids, prix, chargeUtile, nombreEssieux); 
+                        garage.AddVehicle(truck);
+                        break;
+                }
+
                 break;
                                             
             case 3:
-                char immatriculation[10];
                 cin.ignore();
                 cout << "\n\033[32m3 - Rechercher d'un véhicule par immatriculation \n\n\033[0m";
                 cout << "Entrez le numéro d'imatriculation de la voiture : ";
